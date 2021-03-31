2353e3e6af354bee951186c8d0581db72e0ea1ab
Sekhar Routray <sekharroutray@gmail.com>
2021-03-31 13:20:30 +0530

    Update secrets_test_file.sh

--- secrets_test_file.sh
+++ secrets_test_file.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-password = "pbkdf2_sha256$150000$UWB8zYtoLWBB$6YTqsHEoAUWkt272vI448ILc+SLoPiAIX+yb82MFrL8="
+password = "pbkdf2_sha256$150000$UWB8zYtoLWBB$6YTqsHEoAUaajsbdalsjfasbPiAIX+yb82MFrL8="
 password = "test"
